7a3197279f1726217e28a4761c0cea2a648d4716
Cristina-SM <cristinasantanamartindevelop@gmail.com>
2026-02-17 15:27:50 +0000

    Update paths in HTML and JavaScript files

--- src/js/initDarkMode.js
+++ src/js/initDarkMode.js
@@ -1,23 +1,17 @@
-const sunIcon = "src/assets/icons/light-mode.svg";
-const moonIcon = "src/assets/icons/dark-mode.svg";
+const sunIcon = "/src/assets/icons/light-mode.svg";
+const moonIcon = "/src/assets/icons/dark-mode.svg";
 
 export function initDarkMode() {
-    //Obtenemos el botón y el icono del tema
     const darkModeToggle = document.getElementById('darkModeToggle');
     const themeIcon = document.getElementById('darkModeIcon');
 
-    //Si no existen, salimos de la función
     if (!darkModeToggle || !themeIcon) return;
 
     //Almacenamos el tema seleccionado en localStorage
     const savedTheme = localStorage.getItem('theme') || 'light';
 
     themeIcon.src = savedTheme == 'dark' ? sunIcon : moonIcon;
-    if (savedTheme === 'dark') {
-        document.body.classList.add('dark-mode');
-    }
 
-    //Añadimos el evento click al botón para cambiar el tema
     darkModeToggle.addEventListener('click', () => {
         document.body.classList.toggle('dark-mode');
         const isDarkMode = document.body.classList.contains('dark-mode');
--- src/js/initialComponents.js
+++ src/js/initialComponents.js
@@ -3,8 +3,8 @@ import { initDarkMode } from './initDarkMode.js';
 import { authMode } from './authMode.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
-    await loadComponent('header', 'src/components/header.html');
-    await loadComponent('restaurant-item', 'src/components/restaurant-item.html');
+    await loadComponent('header', '/src/components/header.html');
+    await loadComponent('restaurant-item', '/src/components/restaurant-item.html');
     initDarkMode();
     authMode();
 });
