c8ceb492f5b4dfac9226acf53dacb412d3a22312
Cristina-SM <cristinasantanamartindevelop@gmail.com>
2026-02-17 13:06:02 +0000

    Add restaurant item component with styles and integration

--- src/js/initialComponents.js
+++ src/js/initialComponents.js
@@ -4,6 +4,7 @@ import { authMode } from './authMode.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
     await loadComponent('header', 'src/components/header.html');
+    await loadComponent('restaurant-item', 'src/components/restaurant-item.html');
     initDarkMode();
     authMode();
 });
