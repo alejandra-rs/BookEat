0c8ed5b25b055a5c20a7e2df7a512254084b24a7
Cristina-SM <cristinasantanamartindevelop@gmail.com>
2026-02-24 23:02:13 +0000

    header-components bug

--- src/js/date-selector.js
+++ src/js/date-selector.js
@@ -1,10 +1,17 @@
-flatpickr(".flatpickr", {
-    wrap: true,
+const fromPicker = flatpickr("#date-picker", {
     altInput: true,
     altFormat: "d/m/Y",
     dateFormat: "d/m/Y",
     allowInput: true,
-    clickOpens: true,
     minDate: "today",
     locale: {"firstDayOfWeek": 1}
 });
+
+const input_calendar  = document.getElementById("calendar-icon");
+const input_arrow  = document.getElementById("dropdown-arrow");
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("DOMContentLoaded");
+    input_calendar.addEventListener('click', () => fromPicker.open());
+    input_arrow.addEventListener('click', () => fromPicker.open());
+})
