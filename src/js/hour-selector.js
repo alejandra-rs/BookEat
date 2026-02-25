4c6d8ea0654404b909180f48d2096161c3c6b2df
4w4kt <carrona43@gmail.com>
2026-02-25 00:29:25 +0000

    Creating the button with JS in hour-selector.js

--- src/js/hour-selector.js
+++ src/js/hour-selector.js
@@ -1,6 +1,24 @@
+const hoursList = [
+    "09:00 - 10:00",
+    "10:00 - 11:00",
+    "11:00 - 12:00",
+    "12:00 - 13:00",
+    "13:00 - 14:00",
+    "16:00 - 17:00",
+    "17:00 - 18:00"
+];
+const dropdownContainer = document.querySelector('.hour-dropdown');
+
+dropdownContainer.innerHTML = '';
+hoursList.forEach(hour => {
+    const buttonHTML = `<button class="hour-button secondary-button">${hour}</button>`;
+    dropdownContainer.innerHTML += buttonHTML;
+});
+
+
 document.querySelector('.hour-placeholder')
     .addEventListener('click', () => {
-        document.querySelector('.hour-dropdown').classList.toggle('open');
+        dropdownContainer.classList.toggle('open');
     })
 
 
@@ -9,7 +27,7 @@ document.querySelector('.hour-dropdown').addEventListener('click', (event) => {
     const button = event.target.closest('.hour-button');
 
     if (button) {
-        document.querySelector('.hour-dropdown').classList.remove('open');
         document.querySelector('.placeholder').innerText = button.innerText;
+        document.querySelector('.hour-dropdown').classList.remove('open');
     }
 });
