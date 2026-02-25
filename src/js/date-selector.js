336bce25ecc18a1ae3fd4747c652531a43f391cf
Cristina-SM <cristinasantanamartindevelop@gmail.com>
2026-02-25 12:19:30 +0000

    fixed date selector bug in header

--- src/js/date-selector.js
+++ src/js/date-selector.js
@@ -1,17 +1,13 @@
-const fromPicker = flatpickr("#date-picker", {
-    altInput: true,
-    altFormat: "d/m/Y",
-    dateFormat: "d/m/Y",
-    allowInput: true,
-    minDate: "today",
-    locale: {"firstDayOfWeek": 1}
-});
-
-const input_calendar  = document.getElementById("calendar-icon");
-const input_arrow  = document.getElementById("dropdown-arrow");
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("DOMContentLoaded");
-    input_calendar.addEventListener('click', () => fromPicker.open());
-    input_arrow.addEventListener('click', () => fromPicker.open());
-})
+const date_picker = document.getElementById('date-picker');
+if(date_picker) {
+        flatpickr("#date-picker",  {
+                wrap: true,
+                altInput: true,
+                altFormat: "d/m/Y",
+                dateFormat: "d/m/Y",
+                allowInput: true,
+                clickOpens: true,
+                minDate: "today",
+                locale: {"firstDayOfWeek": 1}
+        });
+}
