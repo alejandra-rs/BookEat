a7f57561f23398ac95dabbdc8dec61192567a4c9
Cristina <cristinasantanamartindevelop@gmail.com>
2026-02-27 01:24:54 +0000

    implement dynamic variants for overview component

---
+++ src/templates/overview/setupCards.js
@@ -0,0 +1,10 @@
+async function setupCards(type) {
+    setTimeout(() => {
+        const card = document.querySelectorAll('.overview');
+        console.log(card);
+        for (const x of card) {
+            x.classList.remove('default', 'incoming', 'past', 'restaurant-past', 'restaurant-incoming');
+            x.classList.add(type);
+        }
+    },100)
+}
