80fe65cc5ce6c0d7e294f2856ccd4dfce24b155d
Cristina <cristinasantanamartindevelop@gmail.com>
2026-02-27 10:55:39 +0000

    href go button in overview

--- src/templates/overview/setupCards.js
+++ src/templates/overview/setupCards.js
@@ -1,7 +1,6 @@
 async function setupCards(type) {
     setTimeout(() => {
         const card = document.querySelectorAll('.overview');
-        console.log(card);
         for (const x of card) {
             x.classList.remove('default', 'incoming', 'past', 'restaurant-past', 'restaurant-incoming');
             x.classList.add(type);
