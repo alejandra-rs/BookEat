34f511bfa3a20a3081d21debd464691caf2fd80f
4w4kt <carrona43@gmail.com>
2026-02-27 18:43:28 +0000

    adding the reservations-page and changing the setupCards.js

--- src/templates/overview/setupCards.js
+++ src/templates/overview/setupCards.js
@@ -1,10 +1,10 @@
 
-const Date = {
-    'default': "",
-    'incomingDate': "12/02/2026",
-    'pastDate': "31/12/2025",
-    'restaurantIncomingDate': "12/03/2026",
-    'restaurantPastDate': "04/05/2025",
+const DatesMockUps = {
+    'default-date': "",
+    'incoming-date': "12/02/2026",
+    'past-date': "31/12/2025",
+    'restaurant-incoming-date': "12/03/2026",
+    'restaurant-past-date': "04/05/2025",
 }
 
 const Buttons = {
@@ -34,15 +34,33 @@ const Buttons = {
 async function setupCards(type) {
 let overviews = document.querySelectorAll('.overview');
     for (let overview of overviews) {
-        let date = overview.querySelector(".title-bar > span.date")
-        date.textContent = Date[type + "Date"]
-        let buttonContainer = overview.querySelector(".buttonContainer")
+        let date = overview.querySelector(".overview__content__title > span.date")
+        date.textContent = DatesMockUps[type + "-date"]
+        let buttonContainer = overview.querySelector(".overview__content__button-container")
         buttonContainer.innerHTML = Buttons[type]
-        let card = overview.querySelector(".card-media");
+        let card = overview.querySelector(".overview__image");
         if (type.includes('restaurant')) {
             card.innerHTML = '<img src="/src/assets/icons/account-user.svg" alt="user image" class="profileImage icon_img restaurantUser_overview"/>'
         } else
         card.innerHTML =`<img src="/src/assets/img/restaurant-item.png" alt="a restaurant image" class="restaurantImage icon_img"/>`
     }
-
 }
+
+async function mix() {
+    let index = 0
+    let types = ["incoming", "past", "restaurant-incoming", "restaurant-past"]
+    let overviews = document.querySelectorAll('.overview');
+    for (let overview of overviews) {
+        index = (index +1) % 4
+        let date = overview.querySelector(".overview__content__title > span.date")
+        date.textContent = DatesMockUps[types[index] + "-date"]
+        let buttonContainer = overview.querySelector(".overview__content__button-container")
+        buttonContainer.innerHTML = Buttons[types[index]]
+        let card = overview.querySelector(".overview__image");
+        if (types[index].includes('restaurant')) {
+            card.innerHTML = '<img src="/src/assets/icons/account-user.svg" alt="user image" class="profileImage icon_img restaurantUser_overview"/>'
+        } else
+        card.innerHTML =`<img src="/src/assets/img/restaurant-item.png" alt="a restaurant image" class="restaurantImage icon_img"/>`
+    }
+}
+
