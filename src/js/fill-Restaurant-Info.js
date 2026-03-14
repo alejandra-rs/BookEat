cac9d7e3d835b96c5a2a8f2f0592fddaaee84b81
Cris <75226837+Cristina-SM@users.noreply.github.com>
2026-03-14 20:35:59 +0000

    Update fill-Restaurant-Info.js
    Co-Authored-By: Alejandra Rodríguez <178101850+alejandra-rs@users.noreply.github.com>
    Co-Authored-By: Jose Marcial Galván <144485171+Jose-Marcial-GF@users.noreply.github.com>

--- src/js/fill-Restaurant-Info.js
+++ src/js/fill-Restaurant-Info.js
@@ -1,6 +1,6 @@
-import {fillText, fillImage, fillTotalRating} from "../../src/js/fill-utils.js";
-import {fillReview} from "../../src/js/fill-review.js";
-import {fillMenu} from "../../src/js/fill-menu.js";
+import { fillText, fillImage, fillTotalRating } from "../../src/js/fill-utils.js";
+import { fillReview } from "../../src/js/fill-review.js";
+import { fillMenu } from "../../src/js/fill-menu.js";
 
 export async function fillRestaurantInfo() {
     try {
@@ -15,21 +15,28 @@ export async function fillRestaurantInfo() {
         if (!restaurant) return;
         const container = document.querySelector('.restaurant-info');
 
-        fillImage( 'image', restaurant.images,container);
+        fillImage('image', restaurant.images, container);
         fillText('restaurant-name', restaurant.name, container);
         fillText('description', restaurant.description, container, true);
         fillTotalRating('user-score', restaurant.rating, container);
-        fillImage( 'carousel', restaurant.gallery ?? [restaurant.images]);
+        fillImage('carousel', restaurant.gallery ?? [restaurant.images]);
 
         const allReviews = await revResp.json();
         const restaurantReviews = allReviews.filter(r => r['restaurant-id'] === restaurant.id);
-        const userIdsInRestaurant = new Set(restaurantReviews.map(r => r['user-id']));
+        const previewReviews = restaurantReviews.slice(0, 3);
+        const userIdsInRestaurant = new Set(previewReviews.map(r => r['user-id']));
 
         const allUsers = await userResp.json();
         const userReviews = allUsers.filter(user => userIdsInRestaurant.has(user.id));
-        fillReview(restaurantReviews, userReviews);
+        fillReview(previewReviews, userReviews);
         await fillMenu(restaurant.menu)
-    }catch (error){
+
+        const readMoreBtn = document.querySelector('.restaurant-info-page__main__read-more a');
+        if (readMoreBtn) {
+            readMoreBtn.href = `../../pages/restaurant-reviews-page/restaurant-reviews-page.html?id=${restaurant.id}`;
+        }
+
+    } catch (error) {
         console.error("Error al rellenar el restaurante:", error);
     }
 
