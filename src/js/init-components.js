fad48cbe5d6bad996e3998bfb6c1a5e51e5c29ed
Cris <75226837+Cristina-SM@users.noreply.github.com>
2026-03-14 13:06:13 +0000

    Update init-components.js
    Co-Authored-By: Jose Marcial Galván <144485171+Jose-Marcial-GF@users.noreply.github.com>
    Co-Authored-By: Alejandra Rodríguez <178101850+alejandra-rs@users.noreply.github.com>

--- src/js/init-components.js
+++ src/js/init-components.js
@@ -1,5 +1,21 @@
+import {fillRestaurantItems} from '../../src/js/fill-Restaurant-Items-Carousels.js';
+import {fillRestaurantInfo} from '../../src/js/fill-Restaurant-info.js';
+import {checkDarkMode} from "../../src/js/init-dark-mode.js";
+import {loadTemplate} from "../../src/js/load-template.js";
+import {mix, setupCards} from '../../src/templates/overview/setup-cards.js';
+
 document.addEventListener('DOMContentLoaded', async function() {
     await loadTemplate();
-    await fillRestaurantItems();
+    switch (true) {
+        case window.location.href.includes('landing-page'):
+            await fillRestaurantItems();
+            break;
+        case window.location.href.includes('restaurant-info-page'):
+            await fillRestaurantInfo();
+            break;
+        case window.location.href.includes('reservation-page'):
+            mix();
+            break;
+    }
     checkDarkMode();
 });
