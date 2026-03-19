2b308ff5a24121f0d4c8c135052274cdb0021530
4w4kt <carrona43@gmail.com>
2026-03-19 17:04:51 +0000

    restaurant profile popup implemented

--- src/templates/my-account-popup/log-out.js
+++ src/templates/my-account-popup/log-out.js
@@ -8,3 +8,34 @@ if (logOutBtn) {
         window.location.href = '../../pages/landing-page/landing-page.html';
     });
 }
+
+function adaptarPopupPorRol() {
+    const sessionData = sessionStorage.getItem('currentSession');
+    if (!sessionData) return;
+
+    const session = JSON.parse(sessionData);
+
+    const account = document.getElementById('my-account-popup__profile');
+
+    if (session.rol === 'restaurant') {
+        if (account) account.lastChild.textContent = 'My Restaurant Account';
+
+        const restaurantBtnHTML = `
+            <a href="../../pages/restaurant-info-page/restaurant-info-page.html" id="restaurant-tab">
+                <button class="my-account-popup__nav__button" id="my-restaurant">
+                    <img src="../../assets/icons/fork-knife.svg" alt="Restaurant Icon" class="icon">
+                    My Restaurant
+                </button>
+            </a>
+        `;
+        if (account) {
+            account.insertAdjacentHTML('afterend', restaurantBtnHTML);
+        }
+
+    } else {
+        const restaurantTab = document.getElementById('restaurant-tab');
+        if (restaurantTab) restaurantTab.remove();
+    }
+}
+
+adaptarPopupPorRol();
