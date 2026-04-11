8a523e59eb963d434ade369d5d5289129145d941
Pooul14 <alejandra.rodriguezsilva2000@gmail.com>
2026-04-11 17:38:39 +0100

    Created write review popup (modal).

---
+++ bookeat/src/app/components/insert-image/insert-image.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-insert-image',
+  imports: [],
+  templateUrl: './insert-image.html',
+  styleUrl: './insert-image.css',
+})
+export class InsertImage {}
