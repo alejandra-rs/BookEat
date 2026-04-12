fa3101015195aa2daa0520a2f5224eb228b6ef5f
Cris <75226837+Cristina-SM@users.noreply.github.com>
2026-04-12 01:21:04 +0100

    feature: Create affiliate-form and responsive

---
+++ bookeat/src/app/componentes/affiliate-form/affiliate-form.validators.ts
@@ -0,0 +1,38 @@
+import { email, pattern, required, validate } from '@angular/forms/signals';
+import { PHONE_PATTERN, POSTAL_CODE_PATTERN } from './affiliate-form.constants';
+import { fields } from './affiliate.model';
+
+export function applyAffiliateFormValidators(path: any): void {
+  fields.forEach((field) => {
+    required(path[field], {
+      message: `${field.charAt(0).toUpperCase() + field.slice(1)} is required`,
+    });
+  });
+
+  email(path.email);
+
+  pattern(path.phoneNumber, PHONE_PATTERN, { message: 'Phone number must have exactly 9 digits' });
+
+  validate(path.confirmPassword, ({ value, valueOf }) => {
+    if (value() !== valueOf(path.password)) {
+      return { kind: 'mismatch' };
+    }
+    return undefined;
+  });
+
+  pattern(path.postalCode, POSTAL_CODE_PATTERN, {
+    message: 'Postal Code must have exactly 5 digits',
+  });
+
+  validate(path.tags, ({ value }) => {
+    const tags = value();
+
+    if (!Array.isArray(tags) || tags.length === 0) {
+      return {
+        kind: 'required',
+        message: 'You must add at least one tag.',
+      };
+    }
+    return undefined;
+  });
+}
---
+++ bookeat/src/app/componentes/affiliate-form/affiliate.model.ts
@@ -0,0 +1,45 @@
+export interface AffiliateForm {
+  name: string;
+  surname: string;
+  email: string;
+  phoneNumber: string;
+  password: string;
+  confirmPassword: string;
+  restaurantName: string;
+  addressLine1: string;
+  addressLine2: string;
+  city: string;
+  province: string;
+  postalCode: string;
+  tags: string[];
+}
+export const INITIAL_AFFILIATE_STATE: AffiliateForm = {
+  name: '',
+  surname: '',
+  phoneNumber: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+  restaurantName: '',
+  addressLine1: '',
+  addressLine2: '',
+  city: '',
+  province: '',
+  postalCode: '',
+  tags: [],
+};
+
+export const fields = [
+  'name',
+  'surname',
+  'email',
+  'phoneNumber',
+  'password',
+  'confirmPassword',
+  'restaurantName',
+  'addressLine1',
+  'city',
+  'province',
+  'postalCode',
+  'tags',
+];
