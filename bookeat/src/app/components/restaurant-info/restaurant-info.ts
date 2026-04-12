ef71e80b7e43e8c90162041db018733bb07ef943
Cris <75226837+Cristina-SM@users.noreply.github.com>
2026-04-12 11:52:55 +0100

    feature: implement menu in restaurant info

--- bookeat/src/app/components/restaurant-info/restaurant-info.ts
+++ bookeat/src/app/components/restaurant-info/restaurant-info.ts
@@ -1,24 +1,30 @@
-import { Component, computed, inject, input } from '@angular/core';
+import { Component, computed, inject, input, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Restaurant } from '../../models/restaurant.model';
 import { Category } from '../../models/category.model';
 import { UserScore } from '../user-score/user-score';
+import { MenuPopup } from '../menu-popup/menu-popup';
 import { computeAverageRating } from '../../utils/rating.utils';
 import { CategoriesService } from '../../services/jsonserver/categories.service';
 
 @Component({
   selector: 'app-restaurant-info',
-  imports: [RouterLink, UserScore],
+  imports: [RouterLink, UserScore, MenuPopup],
   templateUrl: './restaurant-info.html',
   styleUrl: './restaurant-info.css',
 })
 export class RestaurantInfo {
   private categoriesService = inject(CategoriesService);
+  @ViewChild(MenuPopup) menuPopup!: MenuPopup;
 
   restaurant = input.required<Restaurant>();
   protected readonly computeAverageRating = computeAverageRating;
 
   categories = computed<Category[]>(() =>
-    this.categoriesService.resolve(this.restaurant().categories)
+    this.categoriesService.resolve(this.restaurant().categories),
   );
+
+  openMenu() {
+    this.menuPopup.open();
+  }
 }
