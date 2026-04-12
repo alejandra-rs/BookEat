815b1b9594ee47a377b75d83ca9431e20e3074c1
Cris <75226837+Cristina-SM@users.noreply.github.com>
2026-04-12 14:00:47 +0100

    feat: adding login to header

--- bookeat/src/app/components/header/header.ts
+++ bookeat/src/app/components/header/header.ts
@@ -1,18 +1,19 @@
-import { Component } from '@angular/core';
-import {DateSelector} from '../date-selector/date-selector';
-import {HourSelector} from '../hour-selector/hour-selector';
-import {DinersSelector} from '../diners-selector/diners-selector';
-import {RouterLink} from '@angular/router';
+import { Component, ViewChild } from '@angular/core';
+import { DateSelector } from '../date-selector/date-selector';
+import { HourSelector } from '../hour-selector/hour-selector';
+import { DinersSelector } from '../diners-selector/diners-selector';
+import { RouterLink } from '@angular/router';
+import { LoginPopup } from '../login-popup/login-popup';
 
 @Component({
   selector: 'app-header',
-  imports: [
-    DateSelector,
-    HourSelector,
-    DinersSelector,
-    RouterLink
-  ],
+  imports: [DateSelector, HourSelector, DinersSelector, RouterLink, LoginPopup],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+  @ViewChild(LoginPopup) loginPopup!: LoginPopup;
+  openLogin() {
+    this.loginPopup.open();
+  }
+}
