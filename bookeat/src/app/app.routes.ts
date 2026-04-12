ac1e7b32f05948e72d2ae40a0cd839ea35ab20a2
4w4kt <carrona43@gmail.com>
2026-04-12 14:09:42 +0100

    feat: footer linked with become an affiliate

--- bookeat/src/app/app.routes.ts
+++ bookeat/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import {LandingPage} from './pages/landing-page/landing-page';
 import {RestaurantInfoPage} from './pages/restaurant-info-page/restaurant-info-page';
+import {AffiliateFormComponent} from './components/affiliate-form/affiliate-form';
 
 export const routes: Routes = [
   {
@@ -16,6 +17,6 @@ export const routes: Routes = [
   {
     path: 'Become-an-affiliate',
     pathMatch: 'full',
-    component: LandingPage
+    component: AffiliateFormComponent
   }
 ];
