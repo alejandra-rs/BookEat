af87169e7c1ea693ba76829cc61961bb345e8625
4w4kt <carrona43@gmail.com>
2026-04-12 13:55:35 +0100

    feat: footer created

--- bookeat/src/app/components/insert-image/insert-image.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-insert-image',
-  imports: [],
-  templateUrl: './insert-image.html',
-  styleUrl: './insert-image.css',
-})
-export class InsertImage {}
